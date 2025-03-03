88e8a42278202c9392e3b1401836442750d23986
French11e <frnzrfr@gmail.com>
2025-03-03 20:19:29 +0800

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">
+        mic check <br /> mic check
+      </h1>
+    </div>
+  );
+}
